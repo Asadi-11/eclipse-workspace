6afe086b40e9317dfb476dc2d221f2de133e30b4
Pavan <asadipankumar05@gmail.com>
2024-07-09 22:55:05 +0530

    Third commit

---
+++ OPPs/src/abstraction/abstract_class.java
@@ -0,0 +1,10 @@
+package abstraction;
+
+public class abstract_class {
+	
+	int a=20, b=40;
+	void medal() {
+		System.out.println(a+b);
+	}
+
+}
---
+++ OPPs/src/abstraction/abstraction_main.java
@@ -0,0 +1,14 @@
+package abstraction;
+
+public class abstraction_main {
+
+	public static void main(String[] args) {
+		
+		
+		abstract_class abs= new abstract_class();
+		
+		abs.medal();
+
+	}
+
+}
